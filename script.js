2a82e43e5960a6df44bebffeecd322d45f659d1b
edwardmavarezdev <edwardmavarezdev@gmail.com>
2022-08-10 16:23:45 -0400

    added sin and cos demostration canvas and circumference draw

--- script.js
+++ script.js
@@ -1,79 +1,57 @@
+//canvas capture
+const canvas = document.getElementById('trigonometric');
+const ctx = canvas.getContext('2d');
 
-//var canvas2 = document.getElementByid('canvas2')
-var canvas = document.getElementById("DemoCanvas");
-var ctx = canvas.getContext("2d");
-//var ctx2 = canvas2.getContext('2d');
+const canvasCircle = document.getElementById('circumference')
+const circle = canvasCircle.getContext('2d')
 
-var c = 0;
-var x = 0;
-var y = 0;
-var interval;
-var speed=4;
+const sin= document.getElementById('sin');
+const sinCtx = sin.getContext('2d');
 
-var direction;
-var lastValueX
-var lastValueY
-var rotateLine
-var angle = 0
-var speed = 40
-var range = 1
-var guideLines=true;
+const cos= document.getElementById('cos');
+const cosCtx = cos.getContext('2d');
 
 
+//circumference corrector
+let lastValueX
+let lastValueY
 
-function drawLineLeft(){
+//interval declaration and direction declaration
+let rotateLine
+let direction;
 
-//		lastValueX = parseInt(Math.cos(angle*Math.PI/180)*150+150)
-//		lastValueY = parseInt(Math.sin(angle*Math.PI/180)*150+150)
-	
+//start angle, speed of update render, and range of degrees move
+let angle = 180
+let speed = 50
+let range = 1
 
-		clearInterval(rotateLine)
-		direction = 'L'
-		rotateLine = setInterval(Move,speed)
-	console.log('')
-}
 
-function drawLineRight(){	
-	
-//		lastValueX = parseInt(Math.cos(angle*Math.PI/180)*150+150)
-//		lastValueY = parseInt(Math.sin(angle*Math.PI/180)*150+150)
-	
+//circumference trigonometrical ecuation fisrt state definition
+let x = parseInt(Math.cos(angle*Math.PI/180)*(0.95*canvas.width/2)+canvas.width/2)
+let y = parseInt(Math.sin(angle*Math.PI/180)*(0.95*canvas.height/2)+canvas.height/2)
 
-		clearInterval(rotateLine)
-		direction = 'R'
-		rotateLine = setInterval(Move,speed)
-	
-}
+// trigonometrical first state definition
+let sinGraphic = [[0,y]];
+let cosGraphic = [[x,0]];
 
-function Move(){
-		
+
+function move(){
+		//erase point element	
 		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-
-		x = parseInt(Math.cos(angle*Math.PI/180)*canvas.width/2+canvas.width/2)
-		y = parseInt(Math.sin(angle*Math.PI/180)*canvas.height/2+canvas.height/2)
-	
 		
-		ctx.beginPath();
-		ctx.strokeStyle='black'
-		ctx.lineWidth='5';
-		ctx.moveTo(canvas.width/2,canvas.height/2);
-		ctx.lineTo(x,y)
-		ctx.lineTo(x,canvas.height/2)
-		ctx.lineTo(canvas.width/2,canvas.height/2);
-		ctx.stroke();
+		//direcion of draw selector
+		if(direction == 'R'){
+			angle+=range}
+		else if (direction == 'L'){
+			angle-=range
+		}else{}
+	
 
-	/*		
-		ctx.beginPath();
-		ctx.strokeStyle='orange';
-		ctx.lineWidth='10';
-		ctx.moveTo(0,0);
-		ctx.lineTo(canvas.width,0)
-		ctx.lineTo(canvas.width,canvas.height)
-		ctx.lineTo(0,canvas.height)
-		ctx.lineTo(0,0)
-		ctx.stroke();
-	*/	
-
+		//setup the position X and Y of the circumference
+		x = parseInt(Math.cos(angle*Math.PI/180)*(0.95*canvas.width/2)+canvas.width/2)
+		y = parseInt(Math.sin(angle*Math.PI/180)*(0.95*canvas.height/2)+canvas.height/2)
+	
+		//draw the point of position
 		ctx.beginPath();
 		ctx.strokeStyle='red';
 		ctx.lineWidth='10';
@@ -81,27 +59,158 @@ function Move(){
 		ctx.lineTo(x+6,y)
 		ctx.stroke();
 
-
-			
-			
-		
-		
-		/*
+		//draw the origin to position
 		ctx.beginPath();
-		ctx.moveTo(lastValueX,lastValueY);
+		ctx.strokeStyle='black'
+		ctx.lineWidth='5';
+		ctx.moveTo(canvas.width/2,canvas.height/2);
 		ctx.lineTo(x,y)
 		ctx.stroke();
 
+		// draw the origin triangle relation 
+		ctx.beginPath();
+		ctx.lineWidth='5';
+		ctx.strokeStyle='green'
+		ctx.moveTo(x,y)
+		ctx.lineTo(x,canvas.height/2)
+		ctx.lineTo(canvas.width/2,canvas.height/2);
+		ctx.stroke();
+		ctx.stroke();
+
+		//draw the indivudual X and Y cordinates representation.
+		
+		//x
+		ctx.beginPath();
+		ctx.strokeStyle='grey';
+		ctx.lineWidth='2';
+		ctx.moveTo(x,y);
+		ctx.lineTo(canvas.width,y);
+		ctx.stroke();
+		
+		//y
+		ctx.beginPath();
+		ctx.strokeStyle='grey';
+		ctx.lineWidth='2';
+		ctx.moveTo(x,y);
+		ctx.lineTo(x,canvas.height);
+		ctx.stroke();
+
+		
+		// draw the circumference in another canvas
+		circle.beginPath();
+		circle.strokeStyle='purple';
+		circle.lineWidth='6';
+		circle.moveTo(lastValueX,lastValueY);
+		circle.lineTo(x,y)
+		circle.stroke();
 		lastValueX = x
 		lastValueY = y
+			
+			
+		//draw sin
+		
+		sinCtx.clearRect(0, 0, sinCtx.canvas.width, sinCtx.canvas.height)
+
+		for(i=sinGraphic.length; i>=2; i--){
+
+			
+			sinCtx.beginPath();
+			sinCtx.strokeStyle='purple';
+			sinCtx.lineWidth='6';
+			sinCtx.moveTo(sinGraphic[i-1][0],sinGraphic[i-1][1]);
+			sinCtx.lineTo(sinGraphic[i-2][0],sinGraphic[i-2][1]);
+			sinCtx.stroke();
+		}
+		
+			
+		sinGraphic.push([0,y])
+			
+		if(sinGraphic.length==500){
+			sinGraphic.shift()
+		}	
+
+		for(l=sinGraphic.length; l>0; l--){
+			sinGraphic[l-1][0]++
+		}
+			
+		//draw cos
 	
-	*/	
-		if(direction == 'R'){
-			angle+=range}
-		else if (direction == 'L'){
-			angle-=range
-		}else{}
-}	
+		cosCtx.clearRect(0, 0, cosCtx.canvas.width, cosCtx.canvas.height)
+		
+		for(i=cosGraphic.length; i>=2; i--){
+
+			
+			cosCtx.beginPath();
+			cosCtx.strokeStyle='purple';
+			cosCtx.lineWidth='6';
+			cosCtx.moveTo(cosGraphic[i-1][0],cosGraphic[i-1][1]);
+			cosCtx.lineTo(cosGraphic[i-2][0],cosGraphic[i-2][1]);
+			cosCtx.stroke();
+		}
+		
+			
+			cosGraphic.push([x,0])
+			
+			if(cosGraphic.length==500){
+				cosGraphic.shift()
+			}	
+
+		for(l=cosGraphic.length; l>0; l--){
+			cosGraphic[l-1][1]++
+		}
+		
+}
+
+
+//buttons
+const clear = document.getElementById('clear')
+const drawLeft = document.getElementById('drawLeft')
+const drawRight = document.getElementById('drawRight')
+const stop = document.getElementById('stop')
+
+//buttons events
+clear.addEventListener('click',erase)
+drawLeft.addEventListener('click',drawLineLeft)
+drawRight.addEventListener('click',drawLineRight)
+stop.addEventListener('click',stopDraw )
+
+//button functions
+
+function erase(){
+	clearInterval(rotateLine)
+	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+	sinCtx.clearRect(0, 0, sinCtx.canvas.width, sinCtx.canvas.height)
+	cosCtx.clearRect(0, 0, cosCtx.canvas.width, cosCtx.canvas.height)
+		
+	circle.clearRect(0, 0, circle.canvas.width, circle.canvas.height)
+
+	sinGraphic = [[0,y]];
+	cosGraphic = [[x,0]];
+}
+
+function drawLineLeft(){
+		clearInterval(rotateLine)
+		direction = 'L'
+		rotateLine = setInterval(()=>{move()},speed)
+}
+
+
+function drawLineRight(){	
+		clearInterval(rotateLine)
+		direction = 'R'
+		rotateLine = setInterval(move,speed)
+	
+}
+
+function stopDraw(){
+
+	clearInterval(rotateLine)
+
+}
+
+
+//old mouse drawer
 /*
 
 	//	if(angle == 360){clearInterval(rotateLine)
@@ -131,43 +240,9 @@ function offMove(){
 }
 */
 
-const clear = document.getElementById('clear')
-const drawLeft = document.getElementById('drawLeft')
-const drawRight = document.getElementById('drawRight')
-const stop = document.getElementById('stop')
 
 
-clear.addEventListener('click',erase)
-
-drawLeft.addEventListener('click',drawLineLeft)
-drawRight.addEventListener('click',drawLineRight)
-
-stop.addEventListener('click',stopDraw )
-
-function erase(){
-
-	clearInterval(rotateLine)
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-
-}
-
-function stopDraw(){
-
-	clearInterval(rotateLine)
-
-}
-
-class line{
-	
-	constructor(x,y){
-		this.x=x;
-		this.y=y;
-	}
-	
-
-
-}
-
+//test angle equivalences
 /*
 
 	0 = 0, c/2;  90 = c/2, 0;   180 = c,c/2;    270 = c/2,c; 
@@ -188,50 +263,8 @@ function testAngle(angle){
 */
 
 
-/*function qwe(){	
-
-		interval = setInterval(()=>{
-
-		if(x<=canvas.width && y<=0){
-			x+=speed
-			console.log(x)
-		}else if(x>=canvas.width && y<=canvas.height){
-			console.log(y)
-			y+=speed
-		}else if(x>=0 && y>=canvas.height){
-			x-=speed
-			console.log(x)
-		}else if(x<=0 && y>=0){
-			y-=speed
-			console.log(y)
-		}else{alert('x '+'y')}
-		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-	
-		ctx.beginPath();
-		ctx.moveTo(centerX,centerY);
-		ctx.lineTo(x,y)
-		ctx.stroke();
-
-		ctx.beginPath();
-		ctx.moveTo(centerX,centerY);
-		ctx.lineTo(x,y)
-		ctx.stroke();
-	 	
-		//console.log(c)
-		c+=1
-		if(c>canvas.width*4){clearInterval(interval)}
-		},20)
-		
-		
-	}
-*/
-	
-
-
-//function rty(){
-	
-//
-
+//test draw and erase
+//function rty(){	
 
 /* class Drop{
         constructor(){
